70c93c2267530c4f5226a30e6a35a47aee3af4f9
Clemens Zeidler <clemens.zeidler@gmail.com>
2024-12-03 21:15:59 +1300

    Validate the blob store path

--- lib/src/blob-store.ts
+++ lib/src/blob-store.ts
@@ -10,16 +10,27 @@ export type BlobStore = {
 export class FileBlobStore implements BlobStore {
   constructor(private baseDir: string[]) {}
 
+  private validatePath(path: string[]) {
+    for (const p of path) {
+      if (p === ".." || p === ".") {
+        throw Error("Invalid path");
+      }
+    }
+  }
+
   async list(path: string[]): Promise<string[]> {
+    this.validatePath(path);
     const fullPath = p.join(...this.baseDir, ...path);
     const content = await fs.readdir(fullPath);
     return content;
   }
   read(path: string[]): Promise<Buffer> {
+    this.validatePath(path);
     const fullPath = p.join(...this.baseDir, ...path);
     return fs.readFile(fullPath);
   }
   async write(path: string[], data: Buffer): Promise<void> {
+    this.validatePath(path);
     const fullPathArray = [...this.baseDir, ...path];
     const fullPath = p.join(...fullPathArray);
     await fs.mkdir(p.join(...fullPathArray.slice(0, -1)), { recursive: true });
