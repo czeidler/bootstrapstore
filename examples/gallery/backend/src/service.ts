8bb5e4a38f29d9a4dda52a65dfecd95e525bee48
Clemens Zeidler <clemens.zeidler@gmail.com>
2024-12-16 21:07:10 +1300

    Add option to pass repo dir as cli argument

--- examples/gallery/backend/src/service.ts
+++ examples/gallery/backend/src/service.ts
@@ -1,3 +1,6 @@
 import { FileBlobStore } from "lib-node";
+import { argv } from "node:process";
 
-export const store = new FileBlobStore(["testRepo"]);
+const repoDir = argv[2] ?? "testRepo";
+
+export const store = new FileBlobStore([repoDir]);
