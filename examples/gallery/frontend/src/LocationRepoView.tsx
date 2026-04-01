88e105b1502e3ffd3831680b036b9420ff606ce7
Clemens Zeidler <clemens.zeidler@gmail.com>
2026-04-01 22:57:40 +1300

    Initial history view

--- examples/gallery/frontend/src/LocationRepoView.tsx
+++ examples/gallery/frontend/src/LocationRepoView.tsx
@@ -1,8 +1,44 @@
-import { LocationInfo, MetadataRepository, rootDir } from "lib";
+import {
+  arrayToHex,
+  LocationInfo,
+  MetadataRepository,
+  Repository,
+  rootDir,
+  VFSDir,
+} from "lib";
 import { FileBrowser } from "./FileBrowser";
 import { Flex, Tabs, Text } from "@mantine/core";
 import { useChildRepo } from "./account-hooks";
 import { useMemo, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+function RepoHistoryFileBrowser({
+  repo,
+  root,
+}: {
+  repo: Repository;
+  root: VFSDir | undefined;
+}) {
+  const { data } = useQuery({
+    queryKey: ["repository", repo.repoId, "commits"],
+    queryFn: () => repo.listCommits(),
+  });
+
+  return (
+    <Flex direction={"row"} gap={5}>
+      <Flex direction={"column"}>
+        {data?.map((it) => (
+          <Text
+            key={arrayToHex(it.hash256)}
+          >{`${it.timestamp.toLocaleString()} ${arrayToHex(it.hash256).slice(0, 8)}, parents: ${it.parents.map((it) => it.slice(0, 8))}`}</Text>
+        ))}
+      </Flex>
+      <Flex direction={"column"} style={{ flexGrow: 1 }}>
+        <FileBrowser root={root} />
+      </Flex>
+    </Flex>
+  );
+}
 
 export function LocationRepoView({
   deviceId,
@@ -39,7 +75,9 @@ export function LocationRepoView({
           <Text>Path: {location.path}</Text>
         </Tabs.Panel>
         <Tabs.Panel value="browse">
-          {repo === undefined ? null : <FileBrowser root={root} />}
+          {repo === undefined ? null : (
+            <RepoHistoryFileBrowser repo={repo} root={root} />
+          )}
         </Tabs.Panel>
       </Tabs>
     </Flex>
